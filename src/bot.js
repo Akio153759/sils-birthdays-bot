67d1bffa5fcb608fe61d0dc6121d4741f72d40ad
Akio Tamashiro <angelakio.tamashiro@gmail.com>
2022-02-22 17:45:14 -0300

    busco el mensaje en la ejecucion del job

--- src/bot.js
+++ src/bot.js
@@ -8,10 +8,9 @@ const schedule = require('node-schedule');
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`);
 
-  const channel = client.channels.cache.find(channel => channel.id === process.env.DISCORD_CHANNEL_ID);
-
   schedule.scheduleJob(process.env.CRON_SCHEDULE_EXPRESSION, () => {
     console.info('Executing job...');
+    const channel = client.channels.cache.find(channel => channel.id === process.env.DISCORD_CHANNEL_ID);
     const birthdays = employees.getBirthdays();
 
     birthdays.forEach(empl => {
