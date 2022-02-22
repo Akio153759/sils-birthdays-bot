72fcca41bca3d1cac46df089b9c3b012e049f3b9
Akio Tamashiro <angelakio.tamashiro@gmail.com>
2022-02-22 16:46:17 -0300

    Agrego schedule diario y la data de los cumples

--- src/bot.js
+++ src/bot.js
@@ -2,22 +2,22 @@ require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const interval = process.env.TIME_INTERVAL;
 const employees = require('./employees');
+const schedule = require('node-schedule');
 
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`);
 
   const channel = client.channels.cache.find(channel => channel.id === process.env.DISCORD_CHANNEL_ID);
 
-  setInterval(() => {
+  schedule.scheduleJob(process.env.CRON_SCHEDULE_EXPRESSION, () => {
+    console.info('Executing job...');
     const birthdays = employees.getBirthdays();
 
     birthdays.forEach(empl => {
-        channel.send(`Feliz cumple ${empl.name}!! Que la pases zarpado :beers: :tada:`);
+      channel.send(`Feliz cumple ${empl.name}!! Que la pases zarpado :beers: :tada:`);
     });
-  }, interval);
-
+  });
 });
 
 client.login(process.env.TOKEN);
